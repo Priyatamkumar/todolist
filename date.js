22b00bb228272b80013fb7286276710a23332a49
priyatam kumar <kpriyatam420@gmail.com>
2023-04-04 11:55:09 +0530

    some changes due to pull

--- date.js
+++ date.js
@@ -1,26 +1,4 @@
 //jshint esversion:6
-<<<<<<< HEAD
-exports.getDate =function (){
-    const today=new Date();
-    const options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-
-    let day=today.toLocaleDateString("en-US", options);
-    return day; 
-}
-exports.getDay= function (){
-    const today=new Date();
-    const options={
-        weekday:"long"
-    };
-    let day=today.toLocaleDateString("en-US",options);
-    return day;
-}
-console.log(exports);
-=======
 
 exports.getDate = function() {
 
@@ -47,4 +25,3 @@ exports.getDay = function () {
   return today.toLocaleDateString("en-US", options);
 
 };
->>>>>>> b2d56a3 (Initial commit)
